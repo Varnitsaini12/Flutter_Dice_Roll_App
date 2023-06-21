99d68ba69bac7802ad01649d693046aa766cdd31
Varnit Saini <varnitsaini2222@gamil.comm>
2023-06-21 11:15:32 +0530

    app_completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'gradient_container.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,21 +14,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-          body: const Center(
-            child: Text(
-              "Hello There!",
-              textScaleFactor: 2.0,
-            ),
+          backgroundColor: const Color.fromRGBO(63, 13, 149, 1),
+          body: const GradientContainer(
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 38, 5, 53),
           ),
           appBar: AppBar(
             backgroundColor: Colors.black,
             title: const Text(
               "Roll Dice",
             ),
+            
           ),
         ),
       ),
     );
   }
 }
+
+
